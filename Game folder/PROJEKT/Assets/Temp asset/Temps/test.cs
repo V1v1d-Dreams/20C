c1979c0db0ac935d2311815807344281e17349a7
Mr.Chewatch <thannawartsinn@gmail.com>
2020-09-02 07:53:03 +0700

    image revealing

---
+++ Game folder/PROJEKT/Assets/Temp asset/Temps/test.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public SpriteRenderer sprite;
+    float var;
+    int num = 0;
+
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+    void Start()
+    {
+        sprite.color = new Color(1f, 1f, 1f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(var);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            num += 1;
+        }
+
+        if (num == 1 && var <= 0.3f)
+        {
+            var += 0.005f;
+        }
+        else if (num == 2 && var > 0.3f && 0.6 >= var)
+        {
+            var += 0.005f;
+        }
+        else if (num == 3 && var > 0.6f && 1 >= var)
+        {
+            var += 0.005f;
+        }
+
+        sprite.color = new Color(1f, 1f, 1f, var);
+    }
+}
---
+++ Game folder/PROJEKT/Assets/Temp asset/Temps/test2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test2 : MonoBehaviour
+{
+    float ver1 = 3.71f;
+    public Animator sss;
+    bool S = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            S = true;
+        }
+       
+        if (S && ver1 > -3.6f)
+        {
+            ver1 -= 0.05f;
+        }
+
+        if (ver1 < 1.54)
+        {
+            sss.SetTrigger("process");
+        }
+
+
+
+        transform.position = new Vector3(transform.position.x, ver1, transform.position.z);
+    }
+}

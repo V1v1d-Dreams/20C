d230f1588043c7b0b7942996516c793298268c9f
Mr.Chewatch <thannawartsinn@gmail.com>
2020-09-23 19:12:15 +0700

    Debugged Game [ 0.1 ]

---
+++ Game folder/PROJEKT/Assets/Temp asset/Temps/Levelloader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Levelloader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitiontime;
+
+    void Update()
+    {
+        
+    }
+
+    public void loadnextLV()
+    {
+        StartCoroutine(loadlevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator loadlevel(int levelindex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitiontime);
+
+        SceneManager.LoadScene(levelindex);
+
+
+    }    
+}

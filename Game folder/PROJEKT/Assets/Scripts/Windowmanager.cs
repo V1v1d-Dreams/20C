197b8c63c0a746260fd2f15257dd5c9aad64c68b
Lyca <supakorn_s@sci.tu.ac.th>
2020-10-14 12:34:55 +0700

    20C [ 0.1.4.0.1 ]
    Menu

---
+++ Game folder/PROJEKT/Assets/Scripts/Windowmanager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Windowmanager : MonoBehaviour
+{
+    Resolution[] resolutions;
+    public Dropdown resolutionDropdown;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        
+        for (int i=0; i< resolutions.Length; i++)
+        {
+            //(resolutions[i].width + "X" + resolutions[i].height).Add(resolutions[i].width + "X" + resolutions[i].height);
+        }
+
+        resolutionDropdown.AddOptions(options);
+    }
+}

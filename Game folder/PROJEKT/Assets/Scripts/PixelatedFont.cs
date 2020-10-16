3c6acb72dc1432c418e0238f15ebfe22fd54c778
Supakorn  Suwannarit <54356230+SupakornIDD@users.noreply.github.com>
2020-10-17 02:34:39 +0700

    20C [ 0.1.5.0.1]
    +Change Resolution
    +FullScreen toggle

---
+++ Game folder/PROJEKT/Assets/Scripts/PixelatedFont.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class PixelatedFont : MonoBehaviour
+{
+        void Start()
+        {
+            GetComponent<Text>().font.material.mainTexture.filterMode = FilterMode.Point;
+        }
+}
+
+//To use pixelated font script
+//1.Import custom font(even if this will be the same original) and set font size to 300 :)
+//2.Set it's "Rendering mode" to "Hinted Raster"
+//3.Add the following script to this text element:
--- Game folder/PROJEKT/Assets/Scripts/Windowmanager.cs
+++ Game folder/PROJEKT/Assets/Scripts/Windowmanager.cs
@@ -2,25 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class Windowmanager : MonoBehaviour
 {
-    Resolution[] resolutions;
+    [Header("Screen Options")]
+    [Tooltip("Put dropdown in here")]
     public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
 
     void Start()
     {
-        resolutions = Screen.resolutions;
+        resolutions = Screen.resolutions.Select(resolution => new Resolution { width = resolution.width, height = resolution.height }).Distinct().ToArray();
 
         resolutionDropdown.ClearOptions();
 
         List<string> options = new List<string>();
-        
+
+        int currentResolutionIndex = 0;
         for (int i=0; i< resolutions.Length; i++)
         {
-            //(resolutions[i].width + "X" + resolutions[i].height).Add(resolutions[i].width + "X" + resolutions[i].height);
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
 
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+    public void quit()
+    {
+        Application.Quit();
     }
 }

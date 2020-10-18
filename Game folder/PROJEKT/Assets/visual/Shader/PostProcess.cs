f0b4be3e525f3a8ffbf5c5a2fd2a6896c3a38dfa
Supakorn  Suwannarit <54356230+SupakornIDD@users.noreply.github.com>
2020-10-18 22:47:41 +0700

    20C [ 0.1.7]
    Shader
    -Inverted color
    -Pixelated images

---
+++ Game folder/PROJEKT/Assets/visual/Shader/PostProcess.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PostProcess : MonoBehaviour
+{
+    public Material effectMaterial;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, effectMaterial);
+    }
+
+}

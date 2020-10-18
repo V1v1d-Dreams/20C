c8abf00a47c4aa47bbffc9e7523064e4603ba2d3
Mr.Chewatch <thannawartsinn@gmail.com>
2020-10-18 16:30:24 +0700

    20C [ 0.1.6]
    - Dialog version mock
    - Lots of bug fix

---
+++ Game folder/PROJEKT/Assets/Scripts/Dialog/TextField.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextField : MonoBehaviour
+{
+    [TextArea(2, 3)]
+    [SerializeField] string[] Dialog;
+    char[] Chararray;
+    [SerializeField] Transform Textstart;
+    [SerializeField] Transform textlocation;
+    [SerializeField] GameObject Char;
+    [SerializeField] int i = 0;
+    [SerializeField] GameObject[] CharsObj;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            textlocation.position = new Vector3(Textstart.position.x , Textstart.position.y,textlocation.position.z);
+            if (i < Dialog.Length)
+            {
+                this.GetComponent<Textmanager>().NewtextPos = 0;
+
+                //find all letter and for loop delete
+                CharsObj = GameObject.FindGameObjectsWithTag("letter");
+                for (int A = 0; A < CharsObj.Length; A++)
+                {
+                    Destroy(CharsObj[A]);
+                }
+
+                Chararray = Dialog[i].ToCharArray();
+                for (int iNdex = 0; iNdex < Chararray.Length; iNdex++)
+                {
+                    textlocation.position = new Vector3(this.GetComponent<Textmanager>().DisplayDialog(Chararray[iNdex], textlocation, Char), textlocation.position.y, textlocation.position.z);
+                }
+                i++;
+            }
+        }
+    }
+
+}
---
+++ Game folder/PROJEKT/Assets/Scripts/Dialog/Textmanager.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Textmanager : MonoBehaviour
+{
+    [System.NonSerialized] public char[] chars = "!#$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]_`{|}~abcdefghijklmnopqrstuvwxyz ".ToCharArray();
+    [SerializeField] Sprite[] charsprite;
+    [SerializeField] Dictionary<char, Chardata> chardata;
+    [SerializeField] Texture2D charSheet;
+    [SerializeField] int spritesize = 80;
+    [SerializeField] int spriteheigh = 64;
+    [SerializeField] float charlength = 0.1f;
+    [SerializeField] public float NewtextPos;
+    
+
+
+    void Start()
+    {
+        //GetSubSprites();
+        GetspriteWidths();
+    }
+
+    /*public void GetSubSprites()
+    {
+        Sprite[] subsprite = Resources.LoadAll<Sprite>(charSheet.name);
+        charsprite = subsprite;
+    }*/
+
+    public void GetspriteWidths()
+    {
+        int height = charSheet.height;
+        int width = charSheet.width;
+
+        int charIndex = 0;
+        chardata = new Dictionary<char,Chardata>();
+
+        for (int texCoordY = height; texCoordY > 0; texCoordY -= spriteheigh) //(i = 0; i < 5; i++) up to down
+        {
+            for (int texCoordX = 0; texCoordX < width; texCoordX += spritesize) // left to right
+            {
+                //print(charIndex);
+                int x = 0;
+                int y = 0;
+
+                int min = 0;
+                int max = spritesize;
+
+                //get min width
+                while (min == 0 && x < spritesize)
+                {
+                    for (y = 0; y < spritesize; y++)
+                    {
+                        if (charSheet.GetPixel(texCoordX + x, texCoordY - y).a != 0)
+                        {
+                            min = x;
+                        }
+                    }
+                    x++;
+                }
+                x = spritesize;
+
+                //get maxwidth
+
+                while (max == spritesize && x > 0)
+                {
+                    for (y = 0; y < spritesize; y++)
+                    {
+                        if (charSheet.GetPixel(texCoordX + x, texCoordY - y).a != 0)
+                        {
+                            max = x;
+                        }
+                    }
+                    x--;
+                }
+
+                //calculate
+                int charwidth = max - min + 1;
+                if (charwidth <= spritesize && charIndex < chars.Length)
+                {
+                    char c = chars[charIndex];
+                    Sprite Charsprite = charsprite[charIndex];
+                    chardata.Add(c, new Chardata(charwidth, Charsprite));
+                }
+                ++charIndex;
+            }
+
+        }
+    }
+
+    public struct Chardata
+    {
+        public int width;
+        public Sprite sprite;
+
+        public Chardata(int width, Sprite sprite)
+        {
+            this.width = width;
+            this.sprite = sprite;
+        }
+    }
+
+    public float DisplayDialog(char input, Transform textlocation,GameObject letter)
+    {
+        print("go");
+        GameObject newtext;
+        NewtextPos = textlocation.position.x;
+
+        newtext = Instantiate(letter);
+        newtext.transform.position = new Vector3(textlocation.position.x,textlocation.position.y,textlocation.position.z);
+        newtext.GetComponent<SpriteRenderer>().sprite = chardata[input].sprite;
+        NewtextPos += (chardata[input].width * charlength);
+
+
+        return (NewtextPos);
+    }
+}

f736c11b8443e344a4d941b9cd0ae1e69b5878b1
Mr.Chewatch <thannawatsinn@gmail.com>
2021-03-30 01:33:13 +0700

    20C [0.5.2.0.0.2.0.0.0.2]
    push

--- Assets/Scripts/Mainmenu/ButtonManager1.cs
+++ Assets/Scripts/Mainmenu/ButtonManager1.cs
@@ -61,32 +61,6 @@ public class ButtonManager1 : MonoBehaviour
         Application.Quit();
     }
 
-
-    void resolutionAll()
-    {
-        resolutions = Screen.resolutions.Select(resolution => new Resolution { width = resolution.width, height = resolution.height }).Distinct().ToArray();
-
-        resolutionDropdown.ClearOptions();
-
-        List<string> options = new List<string>();
-
-        int currentResolutionIndex = 0;
-        for (int i = 0; i < resolutions.Length; i++)
-        {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
-
-            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
-            {
-                currentResolutionIndex = i;
-            }
-        }
-
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
-    }
-
     void SetResolution(int resolutionIndex)
     {
         Resolution resolution = resolutions[resolutionIndex];

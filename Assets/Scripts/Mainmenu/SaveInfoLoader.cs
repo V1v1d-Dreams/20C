6ed12462080b1ecf027059cf5fa86c8005b217e1
Mr.Chewatch <thannawatsinn@gmail.com>
2021-04-02 10:38:31 +0700

    20C [0.5.4]
    - Menu sprite
    - save load sprite

--- Assets/Scripts/Mainmenu/SaveInfoLoader.cs
+++ Assets/Scripts/Mainmenu/SaveInfoLoader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
+using TMPro;
 
 public class SaveInfoLoader : MonoBehaviour
 {
@@ -14,12 +15,12 @@ public class SaveInfoLoader : MonoBehaviour
         {
             string jsonstring = File.ReadAllText(Application.persistentDataPath + "/Savedata_" + slotnumber + ".json");
             SaveData save = JsonUtility.FromJson<SaveData>(jsonstring);
-            this.GetComponent<UnityEngine.UI.Text>().text = "day : " + save.Day + "\n" +
+            this.GetComponent<TMPro.TextMeshProUGUI>().text = "day : " + save.Day + "\n" +
                                                             "money : " + save.money;
         }
         else
         {
-            this.GetComponent<UnityEngine.UI.Text>().text = "new game";
+            this.GetComponent<TMPro.TextMeshProUGUI>().text = "new game";
         }
     }
 
